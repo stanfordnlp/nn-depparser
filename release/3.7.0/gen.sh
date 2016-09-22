e208cb9167c414138aabd84e423abd1df4587db6
Danqi Chen <danqi@cs.stanford.edu>
2016-09-21 17:42:04 -0700

    Replace Makefile by gen.sh

---
+++ release/3.7.0/gen.sh
@@ -0,0 +1,55 @@
+DIR=/u/nlp/data/dependency_treebanks/corenlp-3.7.0
+UD_TREEBANK_DIR=/u/nlp/data/dependency_treebanks/UD/1.3/
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 (english-wsj|english|chinese|french|german|spanish)" >&2
+  exit 1
+fi
+
+# english-wsj
+if [ $1 = "english-wsj" ]; then
+    for split in train dev test; do
+	    echo generate ${split}..
+	    java -mx1g edu.stanford.nlp.trees.UniversalEnglishGrammaticalStructure -basic -keepPunct -conllx -treeFile /u/nlp/data/dependency_treebanks/PTB/tree_files/${split}.mrg > ${DIR}/$1/${split}.gold.conll
+    done 
+fi
+
+# english
+if [ $1 = "english" ]; then
+    for split in train dev test; do
+        echo generate ${split}..
+        java -mx1g edu.stanford.nlp.trees.UniversalEnglishGrammaticalStructure -basic -keepPunct -conllx -treeFile /u/nlp/data/dependency_treebanks/PTB/tree_files/${split}.mrg > ${DIR}/$1/${split}.gold.conll
+    done
+    java -mx1g edu.stanford.nlp.trees.UniversalEnglishGrammaticalStructure -basic -keepPunct -conllx -treeFile /u/nlp/data/dependency_treebanks/extraTrain/extraTrain.mrg >> ${DIR}/$1/train.gold.conll
+fi
+
+# chinese
+if [ $1 = "chinese" ]; then
+    echo 'TODO'
+fi
+
+# french
+if [ $1 = "french" ]; then
+    for split in train dev test; do
+	    echo generate ${split}..
+	    ln -s ${UD_TREEBANK_DIR}/UD_French/fr-ud-${split}.conllu ${DIR}/$1/${split}.gold.conll
+    done
+fi
+
+# german
+if [ $1 = "german" ]; then
+    for split in train dev test; do
+    	echo generate ${split}..
+    	ln -s ${UD_TREEBANK_DIR}/UD_German/de-ud-${split}.conllu ${DIR}/$1/${split}.gold.conll
+    done
+fi
+
+# spanish
+if [ $1 = "spanish" ]; then
+    for split in train dev test; do
+	    echo generate ${split}..
+    	ln -s ${UD_TREEBANK_DIR}/UD_Spanish/es-ud-${split}.conllu ${DIR}/$1/${split}.gold.conll
+    done
+fi
+
+
