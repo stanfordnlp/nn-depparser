26aa801c52dd1db5526bdc163778ae0560eca10e
Danqi Chen <danqi@cs.stanford.edu>
2016-09-26 01:09:49 -0700

    Remove redundant lines when converting CoNLL-X data

--- scripts/mate-parser/convert_data.py
+++ scripts/mate-parser/convert_data.py
@@ -13,11 +13,21 @@ if len(sys.argv) != 2:
 # Open File
 f = open(sys.argv[1],'rt');
 wrds = ""; pos = ""; labs = ""; par = "";
+n_blank = 0
+n_line = 0
 for line in f:
     sent = line.split();
-    if len(sent) > 0:
+    if (len(sent) == 10):
+        if ('-' in sent[0]) or ('#' in sent[0]):
+            continue
         print sent[0] + "\t" + sent[1] + "\t_\t_\t" + sent[4] + "\t_\t_\t_\t" + sent[6] + "\t_\t" + sent[7] + "\t_\t_\t_";
+        n_line += 1
+        n_blank = 0
     else:
-        print "";
+        n_blank += 1
+        if (n_blank == 1) and (n_line > 0):
+            n_line += 1
+            print "";
 f.close();
+
 
